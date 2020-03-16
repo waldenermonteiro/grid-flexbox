61f9d334212482445621637dbcd0f0444e377bde
Waldener Monteiro <jjrr019@gmail.com>
2020-03-16 15:07:15 -0300

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <div class="row" style="margin-left:-8px">
+      <div class="col-3 col-gutter">
+        <div class="row no-wrap">
+          <input type="text" style="width:100%" class="" />
+        </div>
+      </div>
+      <div class="col-4 col-gutter">
+        <div class="row no-wrap">
+          <input type="text" style="width:100%" class="" />
+        </div>
+      </div>
+      <div class="col-5 col-gutter">
+        <div class="row no-wrap">
+          <input type="text" style="width:100%" class="" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld"
+};
+</script>
+
+<style></style>
